ac34e250b56e4df8703e0e1e1d0345280c23c526
avi-codesmith <avi.codesmith@gmail.com>
2025-04-05 13:45:07 +0530

    Implement the Project

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ const img = document.querySelector("img");
 const btn = document.querySelector("button");
 const imgW = document.querySelector(".img-wrapper");
 const loader = document.createElement("div");
+const isValidDomain = (url) => {
+  return /\.(com|in|app|uk|org|net|info|co|io|gov|edu|me|xyz)$/i.test(url);
+};
 
 let sound = new Audio("errorSound.mp3");
-console.log(sound);
 
 loader.classList.add("loader");
 loader.style.margin = "10px 0";
@@ -19,36 +21,48 @@ const add = () => {
 };
 
 const qrGen = () => {
-  if (input.value !== "") {
+  const value = input.value.trim();
+
+  if (value !== "") {
+    if (!isValidDomain(value)) {
+      sound.play();
+      input.classList.add("move");
+      label.textContent = "Enter valid domain";
+
+      setTimeout(() => {
+        input.classList.remove("move");
+      }, 300);
+
+      return;
+    } else {
+      label.textContent = "Enter text or URL";
+    }
+
     imgW.style.display = "block";
     imgW.appendChild(loader);
 
     img.src =
       `https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=` +
-      encodeURIComponent(input.value);
+      encodeURIComponent(value);
 
     img.onload = () => {
       loader.style.display = "none";
     };
   } else {
     sound.play();
-
     input.classList.add("move");
-    setTimeout(() => {
-      input.classList.remove("move");
-    }, 200);
+    label.style.color = "red";
 
-    label.classList.add("move");
     setTimeout(() => {
-      label.classList.remove("move");
-    }, 200);
+      label.style.color = "#888";
+      input.classList.remove("move");
+    }, 300);
   }
 };
 
 input.addEventListener("click", add);
 label.addEventListener("click", add);
 btn.addEventListener("click", qrGen);
-
 input.addEventListener("keydown", (e) => {
   if (e.key === "Enter") {
     qrGen();
